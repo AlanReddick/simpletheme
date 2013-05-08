86a34706b49da9dbeffb88ca084a8f4a1f73a1ce
Alan Reddick <alan@reddickdesign.com.au>
2013-05-08 15:46:26 +1000

    changes user rooting

--- wp-config.php
+++ wp-config.php
@@ -19,7 +19,7 @@
 define('DB_NAME', 'wordpresstheme');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'rooting');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'Akcidder9');
